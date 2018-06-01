8fe317a1cdab1ee2323858443729d73809bd6e75
Ryan Putman <putman10@me.com>
2018-06-01 11:26:34 -0700

    add script to calculate value and display one of three tracks at the end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,50 @@
-// function add(question1, question2, question3, question4, question5) {
-//   return question1 + question2 + question3 + question4 + question5;
-// };
+function add(q1, q2, q3, q4, q5) {
+  return q1 + q2 + q3 + q4 + q5;
+};
 
 $(document).ready(function() {
   $("#start").click(function() {
     $(".quiz").show();
   });
 
+  $("#question1").change(function() {
+    $("#question2").show();
+  });
+
+  $("#question2").change(function() {
+    $("#question3").show();
+  });
+
+  $("#question3").change(function() {
+    $("#question4").show();
+  });
+
+  $("#question4").change(function() {
+    $("#question5").show();
+  });
+
+  $("#question5").change(function() {
+    $("#submit").show();
+  });
+
+  $("form#quizform").submit(function(event) {
+  var q1 = parseInt($("input:radio[name=q1]:checked").val());
+  var q2 = parseInt($("input:radio[name=q2]:checked").val());
+  var q3 = parseInt($("input:radio[name=q3]:checked").val());
+  var q4 = parseInt($("input:radio[name=q4]:checked").val());
+  var q5 = parseInt($("input:radio[name=q5]:checked").val());
 
 
+  if (add(q1, q2, q3, q4, q5) >= 9) {
+    $('.result').text("Ruby/Rails");
+  } else if (add(q1, q2, q3, q4, q5) >= 6 ) {
+    $('.result').text("CSS/React");
+  } else if (add(q1, q2, q3, q4, q5) === 5) {
+    $('.result').text("C#/.NET");
+  } else {
+    $('.result').text("Answer all the questions");;
+  }
 
-
-  // $("form#quizquestions").submit(function(event) {
-  // var question1 = parseInt($("input:radio[name=question1]:checked").val());
-  // var question2 = parseInt($("input:radio[name=question2]:checked").val());
-  // var question3 = parseInt($("input:radio[name=question3]:checked").val());
-  // var question4 = parseInt($("input:radio[name=question4]:checked").val());
-  // var question5 = parseInt($("input:radio[name=question5]:checked").val());
-  //
-  //
-  // if (add(question1, question2, question3, question4, question5) >= 9) {
-  //   $('.result').text("Margot Tenenbaum");
-  // } else if (add(question1, question2, question3, question4, question5) >= 6 ) {
-  //   $('.result').text("Klaus Daimler");
-  // } else if (add(question1, question2, question3, question4, question5) === 5) {
-  //   $('.result').text("Mr. Fox");
-  // } else {
-  //   $('.result').text("Answer all the questions");;
-  // }
-  //
-  // event.preventDefault();
-  // });
+  event.preventDefault();
+  });
 });
