9a2180781712c499ee0b73ce886b90b66359afcb
Ryan Putman <putman10@me.com>
2018-06-01 10:11:45 -0700

    add jquery to show quiz container on click of Take the Quiz button.

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// function add(question1, question2, question3, question4, question5) {
+//   return question1 + question2 + question3 + question4 + question5;
+// };
+
+$(document).ready(function() {
+  $("#start").click(function() {
+    $(".quiz").text("Goodbye!");
+  });
+
+
+
+
+
+  // $("form#quizquestions").submit(function(event) {
+  // var question1 = parseInt($("input:radio[name=question1]:checked").val());
+  // var question2 = parseInt($("input:radio[name=question2]:checked").val());
+  // var question3 = parseInt($("input:radio[name=question3]:checked").val());
+  // var question4 = parseInt($("input:radio[name=question4]:checked").val());
+  // var question5 = parseInt($("input:radio[name=question5]:checked").val());
+  //
+  //
+  // if (add(question1, question2, question3, question4, question5) >= 9) {
+  //   $('.result').text("Margot Tenenbaum");
+  // } else if (add(question1, question2, question3, question4, question5) >= 6 ) {
+  //   $('.result').text("Klaus Daimler");
+  // } else if (add(question1, question2, question3, question4, question5) === 5) {
+  //   $('.result').text("Mr. Fox");
+  // } else {
+  //   $('.result').text("Answer all the questions");;
+  // }
+  //
+  // event.preventDefault();
+  // });
+});
